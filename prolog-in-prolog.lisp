3701a9663852929d788d5a247ebd151f7f734732
Henrique Muniz <hn.muniz.a@gmail.com>
2017-07-11 00:17:03 -0300

    exercise 11-4

--- prolog-in-prolog.lisp
+++ prolog-in-prolog.lisp
@@ -1,18 +1,20 @@
 (in-package #:cl-prolog)
 
-(<- (prove ?goal) (prove-all (?goal)))
+;(<- (prove ?goal) (prove-all (?goal)))
 
-(<- (prove-a11 nil))
-(<- (prove-all (?goal . ?goals))
-    (clause (<- ?goal . ?body))
-    (concat ?body ?goals ?new-goals)
-    (prove-all ?new-goals))
+;(<- (prove-a11 nil))
+
+;; (<- (prove-all (?goal . ?goals))
+;;     (clause (<- ?goal . ?body))
+;;     (concat ?body ?goals ?new-goals)
+;;     (prove-all ?new-goals))
 
 ;; define the member relation
 
-(<- (clause (<- (mem ?x (?x . ?y)))))
-(<- (clause (<- (mem ?x (? . ?z)) (mem ?x ?z))))
+;(<- (clause (<- (mem ?x (?x . ?y)))))
+;(<- (clause (<- (mem ?x (? . ?z)) (mem ?x ?z))))
 
 ;; test
 
-(?- (prove (mem ?x (1 2 3))))
+;(?- (prove (mem ?x (1 2 3))))
+
